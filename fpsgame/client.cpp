2b5165b18e93141d3a9bee5e8b858077a6234708
Jed- <jed885@gmail.com>
2021-07-14 15:09:13 +0200

    Add ability to set custom ping
    Added vars:
    - pingvalue: custom ping value
    - replaceping: whether the original value should be replaced with the 
    custom one

--- fpsgame/client.cpp
+++ fpsgame/client.cpp
@@ -1363,6 +1363,9 @@ namespace game
 
     extern int deathscore;
 
+	VARP(replaceping, 0, 0, 1);
+	VARP(pingvalue, INT_MIN, 0, INT_MAX);
+
     void parsemessages(int cn, fpsent *d, ucharbuf &p)
     {
         static char text[MAXTRANS];
@@ -1903,7 +1906,12 @@ namespace game
             }
 
             case N_PONG:
-                addmsg(N_CLIENTPING, "i", player1->ping = (player1->ping*5+totalmillis-getint(p))/6);
+				if(replaceping) {
+					getint(p); // ignore value from the server
+					addmsg(N_CLIENTPING, "i", player1->ping = pingvalue);
+				} else {
+                	addmsg(N_CLIENTPING, "i", player1->ping = (player1->ping*5+totalmillis-getint(p))/6);
+				}
                 break;
 
             case N_CLIENTPING:
